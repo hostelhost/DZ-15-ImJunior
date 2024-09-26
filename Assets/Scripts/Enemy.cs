650cf7f8769a0c5a0b0c633592c80c4b8042fbed
Илья <Hostel_2008@mail.ru>
2024-09-26 17:07:50 +0300

    Продвинутая генерация врагов

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,14 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 7.0f;
     private Action<Enemy> _onRelease;
-
     private Purpose _target;
 
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
 
-        if (Vector3.SqrMagnitude(transform.position - _target.transform.position) == 0)        
-            _onRelease?.Invoke(this);
-        
+        if (Vector3.SqrMagnitude(transform.position - _target.transform.position) == 0)
+            _onRelease?.Invoke(this);           
     }
 
     public void Initialize(Material material, Purpose target, Action<Enemy> onRelease)
